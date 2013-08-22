1965d3c972ea53438250a1189cbad87e42802d7a
madoldman <madoldman@madoldman.org>
2013-08-22 16:56:13 +0800

    git init

---
+++ include/pcs.h
@@ -0,0 +1,10 @@
+#ifndef _PCS_H
+#define _PCS_H
+struct pcs_t
+{
+	char *server_url;
+	char *access_token;
+	char *refresh_token;
+	char *mount_point;
+};
+#endif
---
+++ src/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include "pcs.h"
+
+#define ARG_FORMAT "a:r:h"
+
+struct pcs_t* conf = NULL;
+
+static void usage(char* argv0, int status){
+	fprintf(stderr, "usage: %s -a access_token -r refresh_token mountpoint\n", 
+			argv0);
+	free(conf);
+	exit(status);
+}
+
+static void parse_arg(int argc, char **argv)
+{
+	extern int optind;
+	int ch;
+	if(argc < 3){
+		usage(argv[0], EXIT_FAILURE);
+	}
+	while((ch = getopt(argc, argv, ARG_FORMAT)) != -1){
+		switch(ch){
+			case 'a' :
+				conf->access_token = optarg;
+				break;
+			case 'r' :
+				conf->refresh_token = optarg;
+				break;
+			case 'h':
+				usage(argv[0], EXIT_SUCCESS);
+				break;
+			default:
+				usage(argv[0], EXIT_FAILURE);
+		}
+	}
+	if(!conf->access_token || !conf->refresh_token){
+		usage(argv[0], EXIT_FAILURE);
+	}
+	if(argc - optind != 1){
+		usage(argv[0], EXIT_FAILURE);
+	}
+	conf->mount_point = argv[optind];
+}
+
+int main(int argc, char **argv)
+{
+	int ret;
+/*	
+ 	extern int errno;
+	if((ret = daemon(1, 1)) == -1){
+		perror(strerror(errno));
+	}
+*/
+	conf = calloc(1, sizeof(struct pcs_t));
+	if(!conf){
+		perror("no memory!\n");
+		exit(EXIT_FAILURE);
+	}
+	parse_arg(argc, argv);
+	free(conf);
+	return 0;
+}
